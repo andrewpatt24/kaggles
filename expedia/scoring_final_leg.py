9a36165433bdd9fba6c42cfe1efc9b70710b875e
apatterson <andrew.patterson@naturalmotion.com>
2016-05-05 23:43:42 +0100

    Experian work needing to be neatened up

--- expedia/scoring_final_leg.py
+++ expedia/scoring_final_leg.py
@@ -11,11 +11,11 @@ chunksize = 1000
 mclf = joblib.load('models/model.pkl')
 print 'model loaded'
 
-test_x = pd.read_csv('data/complete_test_x_cleaned.csv')
+test_x = pd.read_csv('final_leg.csv')
 print test_x.head()
 test_id = test_x['id']
 print test_id.head()
-test_x.drop('id',1).to_csv('data/complete_test_x_vars.csv',index=False)
+test_x.drop('id',1).to_csv('data/complete_test_x_vars_final_leg.csv',index=False)
 print 'got test ids'
 test_x = None
 
@@ -26,7 +26,7 @@ def get_top_x(probs,n=5):
         X_te_op.append([x[1] for x in sorted(tt,reverse=True)][:n])
     return X_te_op
 
-data = pd.read_csv('data/complete_test_x_vars.csv',iterator=True,chunksize=chunksize)
+data = pd.read_csv('data/complete_test_x_vars_final_leg.csv',iterator=True,chunksize=chunksize)
 print "data read"
 
 def create_kaggle_output(test_id,preds):
@@ -38,7 +38,7 @@ def create_kaggle_output_lines(test_id,preds):
         str_output += create_kaggle_output(tid,preds[i])
     return str_output
 
-with open('data/kaggle_expedia_submission_test.csv','w') as f:
+with open('data/kaggle_expedia_submission_test_final_leg.csv','w') as f:
     f.write('id,hotel_cluster\n')
     for i, chunk in enumerate(data):	
         tpred = mclf.predict_proba(chunk.fillna(-1000))
